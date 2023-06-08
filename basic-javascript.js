2254d132b542952e0e2c255d9697a63ce751088e
Josh <92058789+DevXternal@users.noreply.github.com>
2023-06-07 21:18:13 -0700

    Create basic-javascript.js

---
+++ basic-javascript.js
@@ -0,0 +1,12 @@
+// Javascript makes scripts for website which can allow you to make alerts or open different windows
+
+window.open("https://site.com") // opens tab with site
+window.close() // closes tab
+window.alert("YEET") // makes message
+window.prompt("Enter value") // Prompts user to enter info
+
+// You can also create functions for html stuff
+
+function myFunction() {
+  window.alert("function worked")
+}
