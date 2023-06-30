9380b6f4e730f3c4e1a5689db9a217d7894700a0
Josh <92058789+DevXternal@users.noreply.github.com>
2023-06-30 03:27:10 -0700

    Update basic-lua.lua

--- basic-lua.lua
+++ basic-lua.lua
@@ -2,7 +2,7 @@ print("hello") -- will make message
 
 local thing = "hi" -- Local thing is set to hi
 
-print(hi) -- will make message using the value of thing
+print(thing) -- will make message using the value of thing
 
 if thing == "hi" then -- If thing does not equal the value specified, then it will go to the else part and generate an error
   print("hello") -- makes message
